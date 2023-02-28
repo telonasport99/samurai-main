2afa0275278cd5e6282c7cb6a342e840ec56ab94
telonasport99 <telonasport99@gmail.com>
2023-02-28 22:02:20 +0300

    75

--- src/components/Profile/ProfileInfo/ProfileStatus.tsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.tsx
@@ -26,6 +26,14 @@ class ProfileStatus extends React.Component<any, any> {
     onStatusChange=(e:ChangeEvent<HTMLInputElement>)=>{
         this.setState({status:e.currentTarget.value})
     }
+    componentDidUpdate(prevProps: Readonly<any>, prevState: Readonly<any>, snapshot?: any) {
+        if(prevProps.status !== this.props.status){
+            this.setState({
+                status:this.props.status
+            })
+        }
+    }
+
     render() {
         return (
             <>
