67e7542236e2fdf024082cebeb89c9255c1b339d
telonasport99 <telonasport99@gmail.com>
2023-03-28 21:10:21 +0300

    87

--- src/components/Profile/Mypost/Mypost.tsx
+++ src/components/Profile/Mypost/Mypost.tsx
@@ -1,7 +1,7 @@
-import React, {ChangeEvent} from "react";
+import React from "react";
 import Post from "./Post/Post";
 import s from './My.module.css'
-import { PostsType} from "../../../redux/store";
+import {PostsType} from "../../../redux/store";
 import {Field, reduxForm} from "redux-form";
 import {maxLengthCreator, required} from "../../../utilis/validators";
 import {Textarea} from "../../common/FormsControls/FormsControls";
@@ -11,20 +11,22 @@ type MyPostPropsType = {
     addPost: (values:any) => void
 }
 
-function Mypost(props: MyPostPropsType) {
-    let postElement = props.posts.map(el => <Post key={el.id} likesCount={el.likesCount} id={1} message={el.message}/>)
-    let addpost = (value:any) => {
-            props.addPost(value.newPostText)
+const Mypost = React.memo( (props: MyPostPropsType) => {
+    let postElement = props.posts.map(el => <Post key={el.id} likesCount={el.likesCount} id={1}
+                                                  message={el.message}/>)
+    let addpost = (value: any) => {
+        props.addPost(value.newPostText)
     }
 
     return (<div className={s.content}>
             <div>my posts
-               <AddNewPostFormRedux onSubmit={addpost}/>
+                <AddNewPostFormRedux onSubmit={addpost}/>
             </div>
             {postElement}
         </div>
     )
-}
+})
+
 let maxLength10 = maxLengthCreator(10)
 let AddNewPostForm = (props:any)=>{
     return(<form onSubmit={props.handleSubmit}>
